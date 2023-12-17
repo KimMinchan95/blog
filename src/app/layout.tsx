f957e07f7768df781128fc47f0e617dc451aa0bf
KimMinchan95 <hialscks@gmail.com>
2023-12-17 20:26:08 +0900

    chore : 필요없는 import 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { defaultConfig } from "next/dist/server/config-shared";
 
 const inter = Inter({ subsets: ["latin"] });
 
