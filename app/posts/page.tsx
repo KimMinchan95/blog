837dea7649c515bfe705d1b7cf4daeffc8005f04
KimMinchan95 <hialscks@gmail.com>
2023-12-27 23:49:07 +0900

    feat : contentlayer 테스트

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,5 +1,23 @@
+import { allPosts, Post } from "contentlayer/generated";
+import { compareDesc } from "date-fns";
+
 const Page = () => {
-  return <></>;
+  const posts = allPosts.sort((a: Post, b: Post) => {
+    return compareDesc(new Date(a.date), new Date(b.date));
+  });
+
+  return (
+    <div>
+      <h1>All posts</h1>
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.url}>
+            <a href={post.url}>{post.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Page;
