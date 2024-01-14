33e13f50b45c760520212e5d765ed29ccda3b035
KimMinchan95 <hialscks@gmail.com>
2024-01-14 22:09:13 +0900

    feat : Post에 time 추가

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { allPosts } from "contentlayer/generated";
 import { notFound } from "next/navigation";
+import { format } from "date-fns";
+
 import Mdx from "components/Mdx";
 
 interface Props {
@@ -15,9 +17,12 @@ const Post = async ({ params }: Props) => {
 
   return (
     <article>
-      <div>
+      <div className="mx-auto pt-4 pb-1 mb-3 border-b-black-300 border-b-2">
         <h1 className="text-3xl font-bold">{post.title}</h1>
-        <p>{post.desc}</p>
+        <p className="text-sm">{post.desc}</p>
+        <div className="text-end text-sm">
+          {format(post.date, "yyyy-MM-dd")}
+        </div>
       </div>
       <Mdx code={post.body.code} />
     </article>
