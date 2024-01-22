f6f38a9f1b43a2fe1a866b98ce80b6198a70f2ee
KimMinchan95 <hialscks@gmail.com>
2024-01-22 22:25:37 +0900

    feat : footer 정렬

--- app/layout/Footer.tsx
+++ app/layout/Footer.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+
+import { footerItems } from "./util";
+
 const Footer = () => {
-  return <footer className="w-full max-w-5xl">Footer</footer>;
+  return (
+    <footer className="grid place-content-center w-full max-w-5xl h-16 bg-gray-100">
+      <div className="flex items-center divide-x divide-black text-l">
+        {footerItems.map(({ subject, href, Icon }) => (
+          <div key={subject} className="px-3">
+            <Link href={href} target="_blank" className="flex items-center">
+              <Icon />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
---
+++ app/layout/util.ts
@@ -0,0 +1,14 @@
+import { MailOutlined, GithubOutlined } from "@ant-design/icons";
+
+export const footerItems = [
+  {
+    subject: "email",
+    href: "mailto:hialscks@gmail.com?subject=[문의 메일]&body=내용:",
+    Icon: MailOutlined,
+  },
+  {
+    subject: "github",
+    href: "https://github.com/KimMinchan95",
+    Icon: GithubOutlined,
+  },
+];
