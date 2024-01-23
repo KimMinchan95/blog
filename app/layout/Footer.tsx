b81d53ea1bf82f858d4b3aa9cdc38bd0859ae33f
KimMinchan95 <hialscks@gmail.com>
2024-01-23 21:23:48 +0900

    feat : footer 추가

--- app/layout/Footer.tsx
+++ app/layout/Footer.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import { SmileOutlined } from "@ant-design/icons";
 
 import { footerItems } from "./util";
 
 const Footer = () => {
   return (
-    <footer className="grid place-content-center w-full max-w-5xl h-16 bg-gray-100">
+    <footer className="grid place-content-center w-full max-w-5xl h-16 bg-gray-100 relative">
       <div className="flex items-center divide-x divide-black text-l">
         {footerItems.map(({ subject, href, Icon }) => (
           <div key={subject} className="px-3">
@@ -13,6 +14,10 @@ const Footer = () => {
             </Link>
           </div>
         ))}
+      </div>
+      <div className="flex absolute items-center gap-1 right-1 bottom-1 text-slate-400">
+        Thank you for coming to my blog!
+        <SmileOutlined />
       </div>
     </footer>
   );
