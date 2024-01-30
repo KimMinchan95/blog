e30d7f78eb6789f5cbff946aea82a6a33eb3cc49
KimMinchan95 <hialscks@gmail.com>
2024-01-30 22:02:25 +0900

    feat : post mainpage header

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -6,11 +6,19 @@ import BACKGROUND_WAVE from "public/static/images/background-wave.webp";
 const Page = () => {
   return (
     <div>
-      <Image
-        src={BACKGROUND_WAVE}
-        alt="wave"
-        className="w-full h-80 bg-cover object-center"
-      />
+      <div className="relative">
+        <Image
+          src={BACKGROUND_WAVE}
+          alt="wave"
+          className="w-full h-72 bg-cover object-center"
+        />
+        <div className="flex flex-col w-full gap-1 content-center absolute top-4 left-1/2 -translate-x-1/2 text-center">
+          <h1 className="text-3xl font-bold font-serif">Front-End Deep Dive</h1>
+          <desc className="text-sm italic font-mono">
+            Written by Kim Minchan
+          </desc>
+        </div>
+      </div>
       <ul>
         <Posts />
       </ul>
