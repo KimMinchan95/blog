112f9144121a030d49488429ac1ece789cb37248
KimMinchan95 <hialscks@gmail.com>
2024-02-05 23:17:38 +0900

    chore : 이미지 경로 변경

--- util/data/selectImage.ts
+++ util/data/selectImage.ts
@@ -1,16 +1,16 @@
 import { StaticImageData } from "next/image";
 
-import ALGORITHMS from "public/static/images/banner/ALGORITHMS.png";
-import CSS from "public/static/images/banner/CSS.png";
-import ETC from "public/static/images/banner/ETC.png";
-import HTML from "public/static/images/banner/HTML.png";
-import JAVASCRIPT from "public/static/images/banner/JAVASCRIPT.png";
-import REACT from "public/static/images/banner/REACT.png";
-import RECOIl from "public/static/images/banner/RECOIl.png";
-import REDUX from "public/static/images/banner/REDUX.png";
-import TIL from "public/static/images/banner/TIL.png";
-import TYPESCRIPT from "public/static/images/banner/TYPESCRIPT.png";
-import ERROR from "public/static/images/banner/ERROR.png";
+import ALGORITHMS from "public/images/banner/ALGORITHMS.png";
+import CSS from "public/images/banner/CSS.png";
+import ETC from "public/images/banner/ETC.png";
+import HTML from "public/images/banner/HTML.png";
+import JAVASCRIPT from "public/images/banner/JAVASCRIPT.png";
+import REACT from "public/images/banner/REACT.png";
+import RECOIl from "public/images/banner/RECOIl.png";
+import REDUX from "public/images/banner/REDUX.png";
+import TIL from "public/images/banner/TIL.png";
+import TYPESCRIPT from "public/images/banner/TYPESCRIPT.png";
+import ERROR from "public/images/banner/ERROR.png";
 
 interface ImgUris {
   [name: string]: StaticImageData;
